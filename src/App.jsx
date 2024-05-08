aa799eb6788fb7d679dc736f82c43abba08f25fc
klfm2415 <klfm241500@gmail.com>
2024-05-08 13:33:22 +0900

    Component Practive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
+      {/* 헤더 */}
       <header className="bg-blue-100 px-2 py-4 flex justify-between">
+        {/* 로고 */}
         <div className="bg-purple-100">로고</div>
+        {/* 네비바 */}
         <nav className="hidden md:flex gap-4">
           <button className="button-style">메뉴1</button>
           <button className="button-style">메뉴2</button>
@@ -10,8 +15,10 @@ const App = () => {
         </nav>
         <button className="button-style inline-block md:hidden">...</button>
       </header>
+      {/* 섹션 */}
       <section className="bg-green-100 grow px-2">
         <ul className="bg-red-100 mt-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 justify-items-center gap-4">
+          {/* 카드 */}
           <li className="bg-blue-100 w-44 h-60">card</li>
           <li className="bg-blue-100 w-44 h-60">card</li>
           <li className="bg-blue-100 w-44 h-60">card</li>
@@ -24,7 +31,7 @@ const App = () => {
           <li className="bg-blue-100 w-44 h-60">card</li>
         </ul>
       </section>
-      <footer className="bg-yellow-100 px-2">footer</footer>
+      <Footer />
     </div>
   );
 };
